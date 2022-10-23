1ae0e1862c41e42e77c020b2955125861cce54fb
Andy Tran <andytran636@gmail.com>
2022-10-23 11:45:51 -0600

    boilerplate

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cpsc481_group_5_browser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
