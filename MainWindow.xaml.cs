28e9cf5fb0139653ee98c4739371688bc692735a
Andy Tran <andytran636@gmail.com>
2022-11-10 14:04:49 -0700

    navbuttons mostly functional

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,42 +14,74 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 
 namespace cpsc481_group_5_browser
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> History; // list of webpages visited since the browser was opened
         public MainWindow()
         {
             InitializeComponent();
+            History = new List<string>();
+            LoadPage("https://www.google.ca");
         }
 
-        private void go_Click(object sender, RoutedEventArgs e)
+        // Routing
+        void LoadPage(string Url)
         {
-            
+            searchBox.Text = Url;
+            History.Add(Url);
+            webBrowser.Navigate(Url);
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void back_Click(object sender, RoutedEventArgs e)
         {
-
+            if (this.webBrowser.CanGoBack)
+            {
+                this.webBrowser.GoBack();
+            }
         }
 
-        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        private void forward_Click(object sender, RoutedEventArgs e)
         {
+            if (this.webBrowser.CanGoForward)
+            {
+                this.webBrowser.GoForward();
+            }
+        }
 
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            searchBox.Text = "https://www.google.com".Trim();
+            History.Add("https://www.google.com".Trim());
+            webBrowser.Navigate(searchBox.Text);
         }
 
         private void area_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            if (e.Key == Key.Enter)
+            if (e.Key == Key.Enter && e.IsDown)
             {
-                string WebPage = area.Text.Trim();
-                webBrowser.Navigate(WebPage);
+                string Page = searchBox.Text.Trim();
+                LoadPage(Page);
             }
+        }
+
+        private void webBrowser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            string Url = e.Uri.ToString();
+            searchBox.Text = Url;
+        }
+        // End Routing
+
+        private void lock_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void settings_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
