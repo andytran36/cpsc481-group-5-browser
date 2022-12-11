5abfccd0f859a7676aaf4a7fea444f3b9a5f8433
Kevin Van <vankevin58@gmail.com>
2022-12-11 16:25:22 -0700

    Numeric Up Down worky

--- ChangeUserSetting.xaml.cs
+++ ChangeUserSetting.xaml.cs
@@ -20,9 +20,13 @@ namespace cpsc481_group5_browser
     /// </summary>
     public partial class ChangeUserSetting : UserControl
     {
+        private int _numValue = 0;
+        private int _numValue2 = 0;
         public ChangeUserSetting()
         {
             InitializeComponent();
+            txtNum.Text = _numValue.ToString();
+            txtNum2.Text = _numValue2.ToString();
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -65,6 +69,95 @@ namespace cpsc481_group5_browser
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+
+        //Stack overflow numeric up down
+
+        public int NumValue
+        {
+            get { return _numValue; }
+            set
+            {
+                _numValue = value;
+                txtNum.Text = value.ToString();
+            }
+        }
+
+        public int NumValue2
+        {
+            get { return _numValue2; }
+            set
+            {
+                _numValue2 = value;
+                txtNum2.Text = value.ToString();
+            }
+        }
+
+        private void cmdUp_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue++;
+
+        }
+
+        private void cmdUp_Click2(object sender, RoutedEventArgs e)
+        {
+            NumValue2+=5;
+
+        }
+
+        private void cmdDown_Click(object sender, RoutedEventArgs e)
+        {
+            NumValue--;
+
+        }
+
+        private void cmdDown_Click2(object sender, RoutedEventArgs e)
+        {
+            NumValue2-=5;
+
+        }
+
+        private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum == null)
+            {
+                return;
+            }
+
+            if (NumValue < 0)
+            {
+                NumValue = 0;
+            }
+
+            if (NumValue > 24)
+            {
+                NumValue = 24;
+            }
+
+            if (!int.TryParse(txtNum.Text, out _numValue))
+                txtNum.Text = _numValue.ToString();
+        }
+
+        private void txtNum_TextChanged2(object sender, TextChangedEventArgs e)
+        {
+            if (txtNum2 == null)
+            {
+                return;
+            }
+
+            if (NumValue2 < 0)
+            {
+                NumValue2 = 0;
+            }
+
+            if (NumValue2 > 59)
+            {
+                NumValue2 = 59;
+            }
+
+            if (!int.TryParse(txtNum2.Text, out _numValue2))
+                txtNum2.Text = _numValue2.ToString();
         }
     }
 }
